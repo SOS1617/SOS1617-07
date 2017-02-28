a9ad6fd781f125b33693732bd6941d19047a6ca4
sos161707amr <alvaroelderoma@gmail.com>
2017-02-28 12:29:00 +0000

    la hora

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 var express = require("express");
 var app = express();
-var port = (process.env.PORT || 17789);
-
-
-app.post("/hello",(req,res)=>{
-    res.send("Hello world");
-    console.log("New request arrived");
-});
-
-
+var port = (process.env.PORT || 7117);
+var dateFormat = require('dateformat');
+var time = new Date();
 
 app.listen(port, (err) => {
-    if(!err)
-        console.log("Server initialized on port " +port);
+    if (!err)
+        console.log("Server initialized on port " + port);
     else
-        console.log("ERROR initializing server in port" + port + ": " + err );
+        console.log("An error ocurred while trying to initialize server: " + err);
 });
 
-app.get("/time",(req,res) => {
-    var date = Date();
-    res.send( +date);
+app.get("/time", (req, res) => {
+    res.send("<html><body><h1>" + '"' + dateFormat(time, "dS mmmm 'of' yyyy, HH:MM:ss") + '"' + "</h1></body><html>");
 });
 
-
+app.get("/", (req, res) => {
+    res.send("<html><body><h1><a href=/time>/time</a></h1></body><html>");
+});
