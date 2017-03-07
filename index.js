af3d79f38a63ce5f89dd23ecb8e29ccf7963dc65
sos161707amr <alvaroelderoma@gmail.com>
2017-03-07 10:39:44 +0000

    basic server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 var port = (process.env.PORT || 7117);
 var dateFormat = require('dateformat');
 var time = new Date();
@@ -7,8 +8,8 @@ var time = new Date();
 var horas=time.getHours();
 horas=horas+1;
 time.setHours(horas);
-
 //-----aqui termina
+
 
 app.listen(port, (err) => {
     if (!err)
@@ -17,9 +18,21 @@ app.listen(port, (err) => {
         console.log("An error ocurred while trying to initialize server: " + err);
 });
 
+//Feedback F02: show time in a determinated format
+/*
 app.get("/time", (req, res) => {
     res.send("<html><body><h1>" + '"' + dateFormat(time, "dS mmmm 'of' yyyy, HH:MM:ss ") + '"' + "</h1></body><html>");
 });
+
+app.get("/", (req, res) => {
+    res.send("<html><body><h1><a href=/time>/time</a></h1></body><html>");
+});
+*/
+
+
+//Feedback F03
+
+app.use("/",express.static( path.join(__dirname,"public")));
 
 app.get("/", (req, res) => {
     res.send("<html><body><h1><a href=/time>/time</a></h1></body><html>");
