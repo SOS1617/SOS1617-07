6eaa9545cf9298c3e260a4cf95b5400c42ef303e
sos161707jalf <joseantoniolopezfernandez1994@gmail.com>
2017-04-25 23:37:03 +0000

    jose a dormir

--- public/angularEducation/education-list-ctrl.js
+++ public/angularEducation/education-list-ctrl.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 //Obtengo el modulo y creo el controlador sobre él
-
-/* global Materialize */
-=======
->>>>>>> 75e4d11e826300a8da90e0918e90a28daa9ca89c
 angular
     .module("ManagerApp")
-    .controller("JoseListCtrl",["$scope", "$http","$rootScope", function($scope, $http, $rootScope){
+    .controller("JoseListCtrl",["$scope", "$http", function($scope, $http){
         
         $scope.url = "/api/v1/investEducationStats";
         $scope.apikey="sos07";
+        
+        
 
         console.log("Controller initialized ");
          function refresh(){
@@ -115,150 +112,5 @@ angular
                     $scope.birthRateStats = response.data; 
                 });
         };
-        
-        
-        
-        
-        
-        
-        //--------------------------------------------------------------------------
-        
-var previousPage;
-var nextPage;
-var setPage;
-var aux;
-
-/*angular.module("InvestmentsEducationApp").
-controller("InveListCtrl", ["$scope", "$http", "$rootScope",function($scope, $http, $rootScope) {
-    console.log("Controller initialized");*/
-    
-    if (!$rootScope.apikey) $rootScope.apikey = "sos07";
-
-   // $scope.url = "/api/v2/investmentseducation";
-    $scope.search = {};
-    $scope.searchAdd = {};
-
-    $scope.data = {};	
-    var dataCache = {};
-    $scope.currentPage = 1;
-    $scope.maxPages = 1;
-    $scope.pages = [];
-    $scope.pagesLeft = [];
-    $scope.pagesMid = [];
-    $scope.pagesRight = [];
-	
-	var modifier = "";
-    var properties = "";
-
-    var elementsPerPage = 2;
-
-    function setPagination() {
-        var pagesNearby = 2;
-        $scope.pagesLeft = [];
-        $scope.pagesMid = [];
-        $scope.pagesRight = [];
-        if ($scope.maxPages <= pagesNearby * 2) {
-            for (var i = 1; i <= $scope.maxPages; i++) $scope.pagesLeft.push(i);
-        }
-        else if ($scope.currentPage >= 0 && $scope.currentPage <= pagesNearby) {
-            //console.log("Left");
-            //only left and mid
-            for (var i = 1; i <= pagesNearby; i++) $scope.pagesLeft.push(i);
-            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesMid.push(i);
-        }
-        else if ($scope.currentPage >= $scope.maxPages - pagesNearby + 1 && $scope.currentPage <= $scope.maxPages) {
-            //console.log("Right");
-            //only left and mid
-            for (var i = 1; i <= pagesNearby; i++) $scope.pagesMid.push(i);
-            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesRight.push(i);
-        }
-        else {
-            //console.log("Mid");
-            for (var i = 1; i <= pagesNearby; i++) $scope.pagesLeft.push(i);
-            for (var i = Math.max($scope.currentPage - 1, pagesNearby + 1); i <= Math.min($scope.currentPage + 1, $scope.maxPages - pagesNearby); i++) $scope.pagesMid.push(i);
-            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesRight.push(i);
-            if (($scope.pagesLeft[$scope.pagesLeft.length - 1] == $scope.pagesMid[0] - 1) && ($scope.pagesMid[$scope.pagesMid.length - 1] == $scope.pagesRight[0] - 1)) {
-                //console.log("JOIN BOTH");
-                $scope.pagesMid = $scope.pagesMid.concat($scope.pagesRight);
-                $scope.pagesLeft = $scope.pagesLeft.concat($scope.pagesMid);
-                $scope.pagesMid = [];
-                $scope.pagesRight = [];
-            }
-            else if ($scope.pagesLeft[$scope.pagesLeft.length - 1] == $scope.pagesMid[0] - 1) {
-                //console.log("JOIN MID INTO LEFT");
-                $scope.pagesLeft = $scope.pagesLeft.concat($scope.pagesMid);
-                $scope.pagesMid = [];
-            }
-            else if ($scope.pagesMid[$scope.pagesMid.length - 1] == $scope.pagesRight[0] - 1) {
-                //console.log("JOIN MID INTO RIGHT");
-                $scope.pagesRight = $scope.pagesMid.concat($scope.pagesRight);
-                $scope.pagesMid = [];
-            }
-        }
-    }
-
-    $scope.setPage = function(page) {
-        $scope.currentPage = page;
-        $scope.refreshPage();
-    };
-
-    $scope.previousPage = function() {
-        $scope.currentPage--;
-        $scope.refreshPage();
-    };
-
-    $scope.nextPage = function() {
-        $scope.currentPage++;
-        $scope.refreshPage();
-    };
-
-    $scope.refreshPage = function() {
-        if ($scope.currentPage <= 0) $scope.currentPage = 1;
-        if ($scope.currentPage > $scope.maxPages) $scope.currentPage = $scope.maxPages;
-        setPagination();
-        if (dataCache.length > elementsPerPage) {
-            $scope.data = dataCache.slice(Number(($scope.currentPage - 1) * elementsPerPage), Number(($scope.currentPage) * elementsPerPage));
-        }
-        else {
-            $scope.data = dataCache;
-        }
-    };
-
-    var refresh = $scope.refresh = function() {
-
-       /* var modifier = "";
-        var properties = "";
-        if ($scope.search.country && $scope.search.year) {
-            modifier = "/" + $scope.search.country + "/" + $scope.search.year;
-        }
-        else if ($scope.search.country) {
-            modifier = "/" + $scope.search.country;
-        }
-        else if ($scope.search.year) {
-            modifier = "/" + $scope.search.year;
-        }
-        for (var prop in $scope.searchAdd) {
-            if ($scope.searchAdd.hasOwnProperty(prop) && prop) {
-                properties += prop + "=" + $scope.searchAdd[prop] + "&";
-            }
-        }*/
-
-        $http
-            .get("../api/v2/investmentseducation" + modifier + "?" + "apikey=" + $scope.apikey + "&" + properties)
-            .then(function(response) {
-                $scope.maxPages = Math.max(Math.ceil(response.data.length / elementsPerPage), 1);
-                dataCache = response.data;
-                //console.log(JSON.stringify(dataCache, null, 2));
-                $scope.refreshPage();
-                aux = 1;
-            }, function(response) {
-                $scope.maxPages = 1;
-                dataCache = {};
-                $scope.refreshPage();
-                Materialize.toast('<i class="material-icons">error_outline</i> There is no data available', 4000);
-                /*Materialize.toast('<i class="material-icons">error_outline</i> There is no data available', 4000);
-                $scope.data = {};*/
-                aux = 0;
-            });
-    };
+           
 }]);
