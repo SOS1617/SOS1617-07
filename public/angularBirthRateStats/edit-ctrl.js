070ca82a700ba918eac4c40e76a0df8f91702b69
sos161707jpp <juliopx22@gmail.com>
2017-04-25 17:42:30 +0000

    moddule

--- public/angularBirthRateStats/edit-ctrl.js
+++ public/angularBirthRateStats/edit-ctrl.js
@@ -1,24 +1,70 @@
 angular
     .module("ManagerApp")
-    .controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
-    console.log("Controller initialized (EditCtrl)");
-    
-    var apikey = "sos07";
+    .controller("EditCtrl",["$scope", "$http", function($scope, $http){
+        
+        $scope.url = "/api/v1/birthRateStats";
 
-    function refresh() {
-        $http
-            .get("../api/v1/birthRateStats/" + $routeParams.country + "/" + $routeParams.year + "?" + "apikey=" + apikey)
-            .then(function(response) {
-                $scope.editDataUnit = response.data;
+        console.log("Controller initialized ");
+        
+        //CARGAR DATOS
+        $scope.loadInitialData= function(){
+            $http.get($scope.url+"/loadInitialData?apikey="+$scope.apikey)
+            .then(function(){
+                console.log("Load initial data: OK");
+                refresh();
             });
-    }
+        };
+        
+    function refresh(){
+      
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey )
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
+                    $scope.birthRateStats = response.data;
+                });
+            }   
     
-    $scope.discardData = function() {
-        console.log("Discarding changes and returning back to main view");
-        $location.path('/birthRateStats');
-    };
     
-       $scope.putbirthRateStat = function(){
+    //GET A UN CONJUNTO CON PAGINACIÓN
+        $scope.getDataPag = function(){
+           
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey +"&limit="+ $scope.limit +"&offset="+$scope.offset)
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
+                    $scope.birthRateStats = response.data;
+                });
+            
+        } ;
+        
+    //GET SIN PAGINACION
+        $scope.getData = function(){
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey)
+                .then(function(response){
+                    $scope.birthRateStats = response.data;
+                    console.log( "Showing data "  );
+                    
+
+            });
+                
+      };
+   
+        //MÉTODO PARA AÑADIR UN PAÍS    
+        $scope.addbirthRateStat = function(){
+            $http
+            //$scope.newbirthRateStat guarda el país que le estoy metiendo
+                .post($scope.url+"?apikey="+ $scope.apikey, $scope.newbirthRateStat)
+                .then(function(response){
+                    console.log($scope.newbirthRateStat.country + "stats added." );
+                    refresh();
+                });
+        } ;
+        
+        
+        //MÉTODO PARA MODIFICAR UN PAÍS    
+        $scope.putbirthRateStat = function(){
             $http
             //$scope.newbirthRateStat guarda el birthRateStat que le estoy metiendo
                 .put($scope.url +"/"+ $scope.newbirthRateStat.country + "/" +  $scope.newbirthRateStat.year + "?apikey="+ $scope.apikey, $scope.newbirthRateStat)
@@ -27,37 +73,37 @@ angular
                     refresh();
                 });
         };
-   
-
-    // $scope.editData = function(data) {
-
-    //     var oldCountry = data.oldCountry;
-    //     var oldYear = data.oldYear;
-    //     delete data._id;
-    //     delete data.oldCountry;
-    //     delete data.oldYear;
-
-    //     data.year = Number(data.year);
-    //     $http
-    //         .put("../api/v1/gdp/" + oldCountry + "/" + oldYear + "?" + "apikey=" + $scope.apikey, data)
-    //         .then(function(response) {
-    //             console.log("Data " + data.country + " edited!");
-    //             Materialize.toast('<i class="material-icons">done</i> ' + oldCountry + ' has been edited succesfully!', 4000);
-    //             refresh();
-    //         }, function(response) {
-    //             Materialize.toast('<i class="material-icons">error_outline</i> Error editing data!', 4000);
-    //             refresh();
-    //         });
-    // };
-
-    // $scope.updateContact = function(contact) {
-    //     $http
-    //         .put("api/v1/contacts/" + $routeParams.name, contact)
-    //         .then(function(response) {
-    //             console.log("Contact " + contact.name + " edited!");
-    //             $location.path("/");
-    //         });
-    // };
-
-    refresh();
+        
+        //MÉTODO PARA ELIMINAR TODOS LOS PAISES
+        $scope.deleteAllbirthRateStats = function(){
+            $http
+                .delete($scope.url+"?apikey="+ $scope.apikey)
+                .then(function(response){
+                    console.log("All stats delete");
+                    refresh();
+                });
+        };
+        
+        //MÉTODO PARA BORRAR UN SALARIO
+        $scope.deleteOnebirthRateStat = function(country,year){
+            $http
+                .delete($scope.url +"/"+ country +"/"+ year +"/?apikey="+$scope.apikey)
+                .then(function(response){
+                    console.log("birthRateStat delete: ");
+                    refresh();
+                });
+        } ;
+        
+        
+        //MÉTODO PARA LAS BÚSQUEDAS
+        $scope.searches = function(){
+            $http
+                .get($scope.url+"?apikey="+$scope.apikey+"&from="+$scope.newbirthRateStat.from+"&to="+$scope.newbirthRateStat.to)
+                .then(function(response){
+                    console.log("The btween year: "+$scope.newbirthRateStat.from +" and year "+ $scope.newbirthRateStat.to+ " works correctly");
+                    $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
+                    $scope.birthRateStats = response.data; 
+                });
+        };
+           
 }]);
