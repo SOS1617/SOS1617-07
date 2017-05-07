75712b2f1c4d8993cb99eb56f13c3b321fe7c941
sos161707amr <alvaroelderoma@gmail.com>
2017-05-07 18:47:28 +0000

    vuelta atras

---
+++ data-management-app.js
@@ -0,0 +1,48 @@
+/* global angular */
+angular.module("ManagerApp", ["ngRoute"]).config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "main.html"
+    })
+
+    .when("/investEducationStats", {
+            templateUrl: "angularEducation/education-list.html",
+            controller: "JoseListCtrl"
+        })
+        .when("/investEducationStats/:country/:year", {
+            templateUrl: "angularEducation/education-edit.html",
+            controller: "JoseEditCtrl"
+        })
+        
+        .when("/investEducationStats/chart", {
+            templateUrl: "/angularEducation/education-chart.html",
+            controller: "JoseChartCtrl"
+        })
+    .when("/analytics",{
+        templateUrl: "analytics.html"
+    })    
+
+    .when("/salaries", {
+            templateUrl: "angularSalaries/list.html",
+            controller: "SalaryListCtrl"
+        })
+        .when("/salaries/:country/:year", {
+            templateUrl: "angularSalaries/edit.html",
+            controller: "SalaryEditCtrl"
+        })
+        
+        .when("/salaries/chart", {
+            templateUrl: "/angularSalaries/chart-ctrl.html",
+            controller: "SalaryChartCtrl"
+        })
+
+    .when("/gdp-per-capita", {     //Cambiar julio
+            templateUrl: "gdp-per-capita/list.html",
+            controller: "GdpPerCapitaListCtrl"
+        })
+        .when("/gdp-per-capita/:country/:year", {
+            templateUrl: "gdp-per-capita/edit.html",
+            controller: "GdpPerCapitaEditCtrl"
+        });
+
+    console.log("App initialized and configured");
+});
