080128e991d5238d13ec2532df65e1158cb2df02
sos161707amr <alvaroelderoma@gmail.com>
2017-05-07 17:26:26 +0000

    morris

--- public/angularSalaries/chart-ctrl.js
+++ public/angularSalaries/chart-ctrl.js
@@ -21,7 +21,6 @@ angular
             
             for(var i=0; i<response.data.length; i++){
                 $scope.datos.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
-               
                 $scope.minimumSalary.push(Number($scope.data[i].minimumSalary));
                 $scope.averageSalary.push(Number($scope.data[i].averageSalary));
                 $scope.riskOfPoverty.push(Number($scope.data[i].riskOfPoverty));
@@ -31,7 +30,7 @@ angular
             }
         });    
             
-        console.log("Controller initialized");
+        console.log("Salary chart Controller initialized");
         $http.get("/api/v1/salaries/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
             
             
@@ -120,21 +119,27 @@ angular
                 dashboard.draw(data, options);
             }    
 
- });
+
  
     //morris.js
     
-  var day_data = [
-  $scope.data
-];
-Morris.Bar({
+  Morris.Area({
   element: 'myfirstchart',
-  data: day_data,
-  xkey: $scope.datos,
-  ykeys: $scope.minimumSalary,
-  labels: ['Licensed', 'SORN'],
-  xLabelAngle: 60
+  behaveLikeLine: true,
+  data: [
+    {x: '2011 Q1', y: $scope.riskOfPoverty[0]},
+    {x: '2011 Q2', y: $scope.riskOfPoverty[1]},
+    {x: '2011 Q3', y: $scope.riskOfPoverty[2]},
+    {x: '2011 Q4', y: $scope.riskOfPoverty[3]}
+  ],
+  xkey: 'x',
+  ykeys: ['y'],
+  labels: ['Y']
 });
 
 
+
+
+
+ });
     }]);
