af596096643598c77e6bb33ceee07493f8ea4c6d
sos161707amr <alvaroelderoma@gmail.com>
2017-05-08 19:40:32 +0000

    test e2e salaries

--- test/T01-LoadSalaries.js
+++ test/T01-LoadSalaries.js
@@ -1,8 +1,8 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
 		browser.get('https://sos1617-07.herokuapp.com/#!/salaries/');
-		var salaries = element.all(by.repeater('salary in salaries'));
+		var salaries = element.all(by.repeater('dataUnit in data'));
 
-		expect(salaries.count()).toBeGreaterThan(2);
+		expect(salaries.count()).toBeGreaterThan(1);
 	});
 });
