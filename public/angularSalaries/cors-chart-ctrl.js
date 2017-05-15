d3a417923ff3d830ea2b1592d28e5ba06ba7775f
sos161707jpp <juliopx22@gmail.com>
2017-05-15 17:24:36 +0000

    proxy

--- public/angularSalaries/cors-chart-ctrl.js
+++ public/angularSalaries/cors-chart-ctrl.js
@@ -8,5 +8,14 @@ angular.module("ManagerApp").
 controller("RentCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
     console.log("Controller initialized (RentCtrl)");
 
+    $scope.refresh = function() {
+        $http
+            .get("sos1617-05.herokuapp.com/api/v1/economic-situation-stats?apikey=cinco")
+            .then(function(response) {
+                console.log(JSON.stringify(response.data, null, 2));
+            });
+    };
+    
+
     
 }]);
