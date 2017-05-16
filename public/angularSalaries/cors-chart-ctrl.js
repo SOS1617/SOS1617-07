6c3bf8f209e334f771724e4a4eca71ca8edbce4e
sos161707amr <alvaroelderoma@gmail.com>
2017-05-16 11:51:18 +0000

    heroku

--- public/angularSalaries/cors-chart-ctrl.js
+++ public/angularSalaries/cors-chart-ctrl.js
@@ -1,21 +1,107 @@
-/* global angular */
-/* global Materialize */
-/* global $ */
-/* global google */
-/* global Highcharts */
+angular
+    .module("ManagerApp")
+    .controller("SalaryCorsChartCtrl",["$scope","$http",function ($scope, $http){
+        
+        $scope.apikey = "sos07";
+        $scope.dataEducation = {};
+        $scope.dataWages = {};
+        var dataCacheEducation = {};
+        var dataCacheWages = {};
+        $scope.categorias = [];
+        $scope.categorias1 = [];
+        //G07
+        $scope.investEducationStat = [];
+        $scope.healthExpenditureStat = [];
+        $scope.militaryExpenditureStat =[];
+        //G08
+        $scope.year = [];
+        $scope.varied = [];
 
-angular.module("ManagerApp").
-controller("RentCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
-    console.log("Controller initialized (RentCtrl)");
+        
+       function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
 
-    $scope.refresh = function() {
-        $http
-            .get("sos1617-05.herokuapp.com/api/v1/economic-situation-stats?apikey=cinco")
-            .then(function(response) {
-                console.log(JSON.stringify(response.data, null, 2));
-            });
-    };
-    
+//G07s
+                
+     $http.get("https://sos1617-07.herokuapp.com/api/v1/investEducationStats/?apikey=sos07").then(function(response){
+                
+                dataCacheEducation = response.data;
+                $scope.dataEducation =dataCacheEducation;
+                
+                for(var i=0; i<response.data.length; i++){
+                    $scope.categorias.push($scope.dataEducation[i].country);
+                    $scope.investEducationStat.push(Number($scope.dataEducation[i].investEducationStat));
+                    $scope.healthExpenditureStat.push(Number($scope.dataEducation[i].healthExpenditureStat));
+                    $scope.militaryExpenditureStat.push(Number($scope.dataEducation[i].militaryExpenditureStat));
+                }
+                
+                console.log("Wages: "+$scope.dataEducation);
+                
+              //G08
+              
+            $http.get("/api/v1/wages"+ "?" + "apikey=" + $scope.apikey).then(function(response){
+                
+                dataCacheWages = response.data;
+                $scope.dataWages =dataCacheWages;
+                
+                for(var i=0; i<response.data.length; i++){
+                    $scope.categorias1.push($scope.dataWages[i].province);
+                    $scope.year.push(Number($scope.dataWages[i]["year"]));
+                    $scope.varied.push(Number($scope.dataWages[i]["varied"]));
+                }
+                    console.log("Wages: "+$scope.dataWages);
 
-    
+
+                    ////////////////////////////
+                    ////COMPARATIVA  2017////
+                    ////////////////////////////
+                    Highcharts.chart('container',{
+                        title: {
+                            text: 'Integrated G07 & G08'
+                        },
+                        chart: {
+                            type: 'line'
+                        },
+                        xAxis: {
+                            categories: $scope.categorias
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            floating: true,
+                            backgroundColor: '#FFFFFF',
+                            //align: 'left',
+                            verticalAlign: 'top',
+                            align: 'right',
+                            y: 20,
+                            x: 0
+                        },
+                        tooltip: {
+                            formatter: function () {
+                                return '<b>' + this.series.name + '</b><br/>' +
+                                   this.x + ': ' + this.y;
+                            }
+                        },
+                        series:[{
+                            name: 'investEducationStat',
+                            data: $scope.investEducationStat,
+                        },
+                        {
+                            name: 'healthExpenditureStat',
+                            data: $scope.healthExpenditureStat,
+                        },
+                        {
+                            name: 'militaryExpenditureStat',
+                            data: $scope.militaryExpenditureStat,
+                        },
+                        
+                        {
+                            name: 'Wages Varied',
+                            data: $scope.varied
+                        }]
+                    });});
+         
+     });
+               
+
 }]);
--- public/angularSalaries/proxy-chart-ctrl.js
+++ public/angularSalaries/proxy-chart-ctrl.js
@@ -1,26 +1,103 @@
-/* global angular */
-/* global Materialize */
-/* global $ */
-/* global google */
-/* global Highcharts */
+angular
+    .module("ManagerApp")
+    .controller("SalaryProxyChartCtrl",["$scope","$http",function ($scope, $http){
+        
+        $scope.apikey = "sos07";
+        $scope.dataEducation = {};
+        $scope.dataWages = {};
+        var dataCacheEducation = {};
+        var dataCacheWages = {};
+        $scope.categorias = [];
+        $scope.categorias1 = [];
+        //G08
+        $scope.year = [];
+        $scope.varied = [];
+        $scope.averageWage =[];
+        //G07
+        $scope.year1 = [];
+        $scope.averageSalary = [];
+        $scope.minimumSalary = [];
+        $scope.riskOfPoverty = [];
 
-angular.module("ManagerApp").
-controller("SalaryProxyChartCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
-    console.log("Controller initialized (SalaryProxyChartCtrl)");
+      
 
-    if (!$rootScope.apikey) $rootScope.apikey = "sos07";
+//G07s
+                
+     $http.get("/proxy/salaries").then(function(response){
+                
+                dataCacheEducation = response.data;
+                $scope.dataEducation =dataCacheEducation;
+                
+                for(var i=0; i<response.data.length; i++){
+                    $scope.categorias.push($scope.dataEducation[i].province);
+                    $scope.year.push(Number($scope.dataEducation[i].year));
+                    $scope.varied.push(Number($scope.dataEducation[i].varied));
+                    $scope.averageWage.push(Number($scope.dataEducation[i].averageWage));
+                }
+                
+                console.log("Wages: "+$scope.dataEducation);
+                
+              //G08
+              
+            $http.get("/api/v1/salaries"+ "?" + "apikey=" + $scope.apikey).then(function(response){
+                
+                dataCacheWages = response.data;
+                $scope.dataWages =dataCacheWages;
+                
+                for(var i=0; i<response.data.length; i++){
+                    $scope.categorias1.push($scope.dataWages[i].country);
+                    $scope.year1.push(Number($scope.dataWages[i]["year"]));
+                    $scope.averageSalary.push(Number($scope.dataWages[i]["averageSalary"]));
+                    $scope.minimumSalary.push(Number($scope.dataWages[i]["minimumSalary"]));
+                    $scope.riskOfPoverty.push(Number($scope.dataWages[i]["riskOfPoverty"]));
+                }
+                    console.log("Wages: "+$scope.dataWages);
 
-    $scope.refresh = function() {
-        $http
-            .get("../proxy/salaries")
-            .then(function(response) {
-                console.log(JSON.stringify(response.data, null, 2));
-            });
-    };
-    
 
+                    Highcharts.chart('container',{
+                        title: {
+                            text: 'Integrated G07 & G08'
+                        },
+                        chart: {
+                            type: 'line'
+                        },
+                        xAxis: {
+                            categories: $scope.categorias
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            floating: true,
+                            backgroundColor: '#FFFFFF',
+                            //align: 'left',
+                            verticalAlign: 'top',
+                            align: 'right',
+                            y: 20,
+                            x: 0
+                        },
+                        tooltip: {
+                            formatter: function () {
+                                return '<b>' + this.series.name + '</b><br/>' +
+                                   this.x + ': ' + this.y;
+                            }
+                        },
+                        series:[{
+                            name: 'year',
+                            data: $scope.year,
+                        },
+                        
+                        
+                        {
+                            name: 'averageSalary ',
+                            data: $scope.averageSalary
+                        },
+                        
+                        {
+                            name: 'Risk of Poverty',
+                            data: $scope.riskOfPoverty
+                        }]
+                    });});
+         
+     });
+               
 
-
-    
-    
 }]);
